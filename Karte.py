295cd526f68bc18d8d358c8dc01964955ca5f368
Jannek Link <linkjannek@gmail.com>
2015-11-22 20:47:55 +0100

    reformating code und einfügen von variablen werten

--- Karte.py
+++ Karte.py
@@ -11,11 +11,11 @@ class Feld:
         # die x und y koordinate des feldes im spielfeld
         self.x = x
         self.y = y
-        #Hier werden die Koordinaten für die Grafik berechnet
+        # Hier werden die Koordinaten für die Grafik berechnet
         self.k = (self.x * 3 - 3, self.y * 3 - 3, self.x * 3, self.y * 3)
-        #die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
+        # die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
         self.farbe = farbe
-        #Hier wird eine Grafik erstellt, falls gewünscht, dies tue ich da ich für eine Funktion auch noch Felder erstelle
+        # Hier wird eine Grafik erstellt, falls gewünscht, dies tue ich da ich für eine Funktion auch noch Felder erstelle
         if grafik is True:
             self.grafik = bild.create_rectangle(self.k[0], self.k[1], self.k[2], self.k[3], fill=self.farbe)
 
@@ -28,94 +28,113 @@ class Futterquelle:
         self.pheromone = 0
         self.x = x
         self.y = y
-        #Hier werden die Koordinaten für die Grafik berechnet
+        # Hier werden die Koordinaten für die Grafik berechnet
         self.k = (self.x * 3 - 3, self.y * 3 - 3, self.x * 3, self.y * 3)
-        #die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
-        self.farbe=farbe
-        #Hier wird eine Grafik erstellt
+        # die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
+        self.farbe = farbe
+        # Hier wird eine Grafik erstellt
         self.grafik = bild.create_rectangle(self.k[0], self.k[1], self.k[2], self.k[3], fill=self.farbe)
-
 
 
 # die Nest-Klasse, die Speichert wie viel Futter bereits zum Nest gebracht wurde
 class Nest:
     def __init__(self, x, y, farbe, bild):
+        # das Futter, welches die Ameisen zum Nest gebracht haben
         self.gesammeltes_futter = 0
+        # die Koordinaten des Nestes
         self.x = x
         self.y = y
         # die pheromone, denen die Ameisen nachlaufen
         self.pheromone = 0
-        #Hier werden die Koordinaten für die Grafik berechnet
+        # Hier werden die Koordinaten für die Grafik berechnet
         self.k = (self.x * 3 - 3, self.y * 3 - 3, self.x * 3, self.y * 3)
-        self.farbe=farbe#die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
-        self.grafik = bild.create_rectangle(self.k[0], self.k[1], self.k[2], self.k[3], fill=self.farbe)#Hier wird eine Grafik erstellt
-
+        self.farbe = farbe  # die Farbe, die das Feld am Anfang hat damit es falls es pheromone hatte wieder seine farbe annehmen kann
+        self.grafik = bild.create_rectangle(self.k[0], self.k[1], self.k[2], self.k[3],
+                                            fill=self.farbe)  # Hier wird eine Grafik erstellt
 
 
 # die Karten-Klasse, hier ist das spielfeld abgespeichert und hier wird es erstellt
 class Karte:
-    def __init__(self, nestposition_x, nestposition_y, futterquellen_anzahl,tk):
-
+    def __init__(self, nestposition_x, nestposition_y, futterquellen_anzahl, tk):
+        # hier wird das Canvas erstellt auf dem sich die grafische Darstellung abspielt
         self.karte = Canvas(tk, width=1500, height=1500)
         self.karte.pack()
-        self.nest = Nest(nestposition_x, nestposition_y,"blue",self.karte)
-        self.felder = self.felder_erstellen(500, 500,futterquellen_anzahl)  # die felder bzw. das Spielfeld wird mit verschieden Parametern erstellt
+        self.nest = Nest(nestposition_x, nestposition_y, "blue", self.karte)
+        # die felder bzw. das Spielfeld wird mit verschieden Parametern erstellt
+        self.felder = self.felder_erstellen(500, 500, futterquellen_anzahl)
 
     # Funktion, welche das Spielfeld erstellt
     def felder_erstellen(self, begrenzung_x, begrenzung_y, futterquellen_anzahl):
-        felder = []  # die Liste mit allen Feldern
+        # die Liste mit allen Feldern
+        felder = []
         for x in range(0, begrenzung_x):
-            felder_spalte = []  # für jede Spalte wird wiederum eine Liste erstellt
+            # für jede Spalte wird wiederum eine Liste erstellt
+            felder_spalte = []
             for y in range(0, begrenzung_y):
                 farbe_feld = "green"
-                felder_spalte.append(Feld(x, y, True, farbe_feld,
-                                          self.karte))  # und nun wird diese Spalte mit Feldern gefüllt,welche ihre x un y koordinate als Attribut bekommen
+                # und nun wird diese Spalte mit Feldern gefüllt,welche ihre x un y koordinate als Attribut bekommen
+                felder_spalte.append(Feld(x, y, True, farbe_feld, self.karte))
             felder.append(felder_spalte)
 
         # Jetzt werden bereits bestehende felder durch Futterquellen ersetzt
         for futterquelle in range(0, futterquellen_anzahl):
-            zufaellig_x = rd.randint(0, begrenzung_x-1)  # hier werden zufällige positionen innerhalb der Karte ausgewählt
-            zufaellig_y = rd.randint(0, begrenzung_y-1)
-            farbe_futterquelle="red"
+            # hier werden zufällige positionen innerhalb der Karte ausgewählt
+            zufaellig_x = rd.randint(0, begrenzung_x - 1)
+            zufaellig_y = rd.randint(0, begrenzung_y - 1)
+            farbe_futterquelle = "red"
             while zufaellig_x == self.nest.x and zufaellig_y == self.nest.y and type(
                     # dies ist notwendig damit die Futterquelle nich auf eine bereits bestehende Futterquelle oder das Nest platziert wird
                     felder[zufaellig_x][zufaellig_y]) is Futterquelle:
-                zufaellig_x = rd.randint(0, begrenzung_x-1)
-                zufaellig_y = rd.randint(0, begrenzung_y-1)
-            felder[zufaellig_x][zufaellig_y] = Futterquelle(zufaellig_x,zufaellig_y,farbe_futterquelle,self.karte)  # hier wird das Feld durch die Futterquelle ersetzt
-
+                zufaellig_x = rd.randint(0, begrenzung_x - 1)
+                zufaellig_y = rd.randint(0, begrenzung_y - 1)
+            # hier wird das Feld durch die Futterquelle ersetzt
+            felder[zufaellig_x][zufaellig_y] = Futterquelle(zufaellig_x, zufaellig_y, farbe_futterquelle, self.karte)
         self.karte.delete(felder[self.nest.x][self.nest.y].grafik)
-        felder[self.nest.x][
-            self.nest.y] = self.nest  # zuletzt wird noch das Feld an der Position des Nestes durch das Nest erstzt
+        # zuletzt wird noch das Feld an der Position des Nestes durch das Nest erstzt
+        felder[self.nest.x][self.nest.y] = self.nest
 
         return felder
 
+    # Diese Funktion gibt alle Felder zurück auf die Ameise gehen kann
     def gebe_umliegende_felder(self, feld):
         umliegende_felder = []
-
-        moegliche_felder = [Feld(feld.x + 1, feld.y, False, 0, 0), Feld(feld.x - 1, feld.y, False, 0, 0),
+        # hier werden neue Felder erzeugt da es einen Index out of range Fehler geben würde wenn man Felder aus der
+        # felder-liste aufrufen würde diese sic aber außerhalb der Karte befinden
+        moegliche_felder = [Feld(feld.x + 1, feld.y, False, 0, 0),
+                            Feld(feld.x - 1, feld.y, False, 0, 0),
                             Feld(feld.x, feld.y + 1, False, 0, 0),
                             Feld(feld.x, feld.y - 1, False, 0, 0)]
+        # Jetzt werden die erstellten Felder geprüft ob sie in der Karte sind und die entsprechenden Felder der
+        # felder-liste werden den umliegenden Feldern hinzugefügt
         for moegliches_feld in moegliche_felder:
             if self.pruefe_ob_feldinkarte(moegliches_feld) is True:
                 umliegende_felder.append(self.felder[moegliches_feld.x][moegliches_feld.y])
-
+        # alle existierenden Felder werden zurückgegeben
         return umliegende_felder
 
+    # diese Funktion prüft ob sich ein Feld innerhalb der Karte befindet indem sie überprüft ob die Koordinaten innerhalb
+    # der Grenzwerte liegen
     def pruefe_ob_feldinkarte(self, feld):
         if feld.x < len(self.felder) and feld.x > -1 and feld.y < len(self.felder[1]) and feld.y > -1:
             return True
 
+    # Hier werden die Felder, ausgehend von einem Feld, zurückgegeben, die näher am Nest liegen
     def gib_feld_naeher_nest(self, feld):
         felder_naeher_nest = []
+        # Wenn die x-koordinate nicht mit der x-koordinates des Nestes übereinstimmt
         if feld.x != self.nest.x:
+            # wird wenn der x-wert größer ist ein Feld zurückgegeben mit einem um eins kleineren x wert
             if feld.x > self.nest.x:
                 felder_naeher_nest.append(self.felder[feld.x - 1][feld.y])
+            # wird wenn der x-wert kleiner ist ein Feld zurückgegeben mit einem um eins größeren x wert
             if feld.x < self.nest.x:
                 felder_naeher_nest.append(self.felder[feld.x + 1][feld.y])
+        # Wenn die y-koordinate nicht mit der y-koordinates des Nestes übereinstimmt
         if feld.y != self.nest.y:
+            # wird wenn der y-wert größer ist ein Feld zurückgegeben mit einem um eins kleineren y wert
             if feld.y > self.nest.y:
                 felder_naeher_nest.append(self.felder[feld.x][feld.y - 1])
+            # wird wenn der y-wert kleiner ist ein Feld zurückgegeben mit einem um eins größeren y wert
             if feld.y < self.nest.y:
                 felder_naeher_nest.append(self.felder[feld.x][feld.y + 1])
         return felder_naeher_nest
--- Standardschleife.py
+++ Standardschleife.py
@@ -2,48 +2,63 @@
 
 import Karte
 import random as rd
-from tkinter import*
+from tkinter import *
 
 
 # Die Hauptschleife in der alle Schritte durchgeführt werden
 def __main__():
+    # nötig damit die while-Schleife beendet wird
+    hoereAuf = False
 
-    hoereAuf = False#nötig damit die while-Schleife beendet wird
     def wennSchliessenGedrueckt():
-        nonlocal hoereAuf#macht die Veränderung der variablen hoereAuf nicht nur lokal für dies Funkrion
+        # macht die Veränderung der variablen hoereAuf nicht nur lokal für dies Funktion
+        nonlocal hoereAuf
         hoereAuf = True
 
-    tk=Tk()
+    tk = Tk()
     tk.protocol("WM_DELETE_WINDOW", wennSchliessenGedrueckt)
+    # Die Liste in der alle Ameisenobjekte vermerkt sind um jederzeit abrufbar zu sein
+    ameisenliste = []
+    # die variablen Nestkoordinaten
+    nestposition_x = int(input("Welche x-koordinate soll das Nest haben?"))
+    nestposition_y = int(input("Welche y-koordinate soll das Nest haben?"))
+    # die variable Futterquellenanzahl
+    futterquellen_anzahl = int(input("Wie viele Futterquellen soll es geben?"))
 
-    ameisenliste = []  # Die Liste in der alle Ameisenobjekte vermerkt sind um jederzeit abrufbar zu sein
-    nestposition_x = 249  # die variablen Nestkoordinaten
-    nestposition_y = 250
-    futterquellen_anzahl = 500  # die variable Futterquellenanzahl
-    spielfeld = Karte.Karte(nestposition_x, nestposition_y,
-                            futterquellen_anzahl,tk)  # Das spielfeld auf dem sich die Ameisen bewegen
-    ameisenzahl = 300  # die variable Ameisenanzahl
-    ameisenliste_fuellen(spielfeld, ameisenliste,
-                      ameisenzahl)  # Hier wird die Funktion aufgerufen, welche di Ameisen erstellt
-    verdunstungszeit = 75  # die verdunstungszeit gibt an wie viele Runden benötigt werden um einen Pheromonpunkt abzubauen
+    # die variable Ameisenanzahl
+    ameisenzahl = int(input("Wie viele Ameisen soll es geben?"))
+    # die verdunstungszeit gibt an wie viele Runden benötigt werden um einen Pheromonpunkt abzubauen
+    verdunstungszeit = int(input("Wie viele Runden soll es dauern bis ein pheromonpunkt abgebaut ist?"))
+    # Das spielfeld auf dem sich die Ameisen bewegen
+    spielfeld = Karte.Karte(nestposition_x, nestposition_y, futterquellen_anzahl, tk)
 
-    while (not hoereAuf):# nachdem alle Objekte erstellt sind beginnt die Simulation in einer schleife
-        bewege(spielfeld, ameisenliste)#Zuerst werden alle Ameisen bewegt
-        pheromon_update(spielfeld, verdunstungszeit)#dann verdunstet ein Teil der Pheromone
+    # Hier wird die Funktion aufgerufen, welche di Ameisen erstellt
+    ameisenliste_fuellen(spielfeld, ameisenliste, ameisenzahl)
+
+    # nachdem alle Objekte erstellt sind beginnt die Simulation in einer schleife
+    while (not hoereAuf):
+        # Zuerst werden alle Ameisen bewegt
+        bewege(spielfeld, ameisenliste)
+        # dann verdunstet ein Teil der Pheromone
+        pheromon_update(spielfeld, verdunstungszeit)
         tk.update()
+    # Zerstört das grafik-Fenster
+    tk.destroy()
 
-    tk.destroy()#Zerstört das grafik-Fenster
 
-
-
-def bewege(spielfeld, ameisen):#Die Funktion in der Jede Ameise einzeln bewegt wird
+# Die Funktion in der Jede Ameise einzeln bewegt wird
+def bewege(spielfeld, ameisen):
     for ameise in ameisen:
-        felder_zu_nest = spielfeld.gib_feld_naeher_nest(ameise.feld)#Hier wird die Funktion Aufgerufen, die alle Spielferder gibt, die von der Ameise aus näher zum Nest sind
-        if ameise.futter is False:#Hier wird das Feld der Ameise berechnet wenn sie kein Futter hat
-            moegliche_felder = spielfeld.gebe_umliegende_felder(ameise.feld)#zuerst sind alle Felder möglich die um die herum sind
-            pheromon_felder = []#das ist die Liste mit den Feldern mit den meisten Pheromonen
-
-            for moegliches_feld in moegliche_felder:#Hier werden die Felder mit den meisten Pheromonen aus den umliegenden Feldern herausgesucht
+        # Hier wird die Funktion Aufgerufen, die alle Spielferder gibt, die von der Ameise aus näher zum Nest sind
+        felder_zu_nest = spielfeld.gib_feld_naeher_nest(ameise.feld)
+        # Hier wird das Feld der Ameise berechnet wenn sie kein Futter hat
+        if ameise.futter is False:
+            # zuerst sind alle Felder möglich die um die herum sind
+            moegliche_felder = spielfeld.gebe_umliegende_felder(ameise.feld)
+            # das ist die Liste mit den Feldern mit den meisten Pheromonen
+            pheromon_felder = []
+            # Hier werden die Felder mit den meisten Pheromonen aus den umliegenden Feldern herausgesucht
+            for moegliches_feld in moegliche_felder:
                 if len(pheromon_felder) == 0:
                     pheromon_felder.append(moegliches_feld)
                 elif moegliches_feld.pheromone > pheromon_felder[0].pheromone:
@@ -52,63 +67,91 @@ def bewege(spielfeld, ameisen):#Die Funktion in der Jede Ameise einzeln bewegt w
                     pheromon_felder.append(moegliches_feld)
                 elif moegliches_feld.pheromone == pheromon_felder[0].pheromone:
                     pheromon_felder.append(moegliches_feld)
-            for pheromon_feld in pheromon_felder:#Nun werden aus den Pheromon feldern alle Felder, die zum Nest hingehen, entfernt
-                if pheromon_feld in felder_zu_nest and pheromon_feld.pheromone>0:
+            # Nun werden aus den Pheromon feldern alle Felder, die zum Nest hingehen, entfernt
+            for pheromon_feld in pheromon_felder:
+                if pheromon_feld in felder_zu_nest and pheromon_feld.pheromone > 0:
                     pheromon_felder.remove(pheromon_feld)
-            if len(pheromon_felder) == 0:#Wenn aber nun keine Felder mehr in der Liste sind
-                pheromon_felder = moegliche_felder#stehen nun durch diese zuweisung alle Spielfelder zur verfügung und
-
-            ameise_feld = rd.choice(pheromon_felder)#ein zufälliges Feld aus den pheromon_feldern wird ausgewählt und dem Ameisen Feld zugewiesen
-                                                    #dies mache ich damit ich später die Grafik der Ameise bewegen kann
-            if type(ameise_feld) is Karte.Futterquelle:#Wenn die Ameise nun eine Futterquelle erricht hat
-                if ameise.feld.futter > 0:# Wenn die Futterquelle noch Futter hat
-                    ameise.futter = True#Nimmt die Ameise ein Futter auf
+            # Wenn aber nun keine Felder mehr in der Liste sind
+            if len(pheromon_felder) == 0:
+                # stehen nun durch diese zuweisung alle Spielfelder zur verfügung und
+                pheromon_felder = moegliche_felder
+            # ein zufälliges Feld aus den pheromon_feldern wird ausgewählt und dem Ameisen Feld zugewiesen
+            # dies mache ich damit ich später die Grafik der Ameise bewegen kann
+            ameise_feld = rd.choice(pheromon_felder)
+            # Wenn die Ameise nun eine Futterquelle erricht hat
+            if type(ameise.feld) is Karte.Futterquelle:
+                # Wenn die Futterquelle noch Futter hat
+                if ameise.feld.futter > 0:
+                    # Nimmt die Ameise ein Futter auf
+                    ameise.futter = True
                     ameise.feld.futter -= 1
-        elif ameise.futter is True:#Wenn die Ameise bereits Futter geladen hat
-            moegliche_felder = felder_zu_nest#sind die moeglichen Felder alle Felder, die näher zum Nest sind
-            ameise_feld = rd.choice(moegliche_felder)#davon wird ein zufälliges ausgewählt
-
-        if ameise.futter is True:#Nun wird dem Feld ein Pheromon-Punkt hinzugefügt, falls die Ameise ein Futter hat
+        # Wenn die Ameise bereits Futter geladen hat
+        elif ameise.futter is True:
+            # sind die moeglichen Felder alle Felder, die näher zum Nest sind
+            moegliche_felder = felder_zu_nest
+            # davon wird ein zufälliges ausgewählt
+            ameise_feld = rd.choice(moegliche_felder)
+        # Nun wird dem Feld ein Pheromon-Punkt hinzugefügt, falls die Ameise ein Futter hat
+        if ameise.futter is True:
             ameise.feld.pheromone += 1
-        spielfeld.karte.move(ameise.grafik,3*(ameise_feld.x-ameise.feld.x),3*(ameise_feld.y-ameise.feld.y))#Hier wird die Grafik der Ameise bewegt
-        ameise.feld=ameise_feld#Der Ameise wird ein neues Feld zugewiesen
-        if type(ameise.feld) is Karte.Nest and ameise.futter is True:#Jetzt legt die Ameise ihr Futter ab wenn sie das Nest erreicht hat
-                ameise.futter = False
-                spielfeld.nest.gesammeltes_futter += 1
+        # Hier wird die Grafik der Ameise bewegt
+        spielfeld.karte.move(ameise.grafik, 3 * (ameise_feld.x - ameise.feld.x), 3 * (ameise_feld.y - ameise.feld.y))
+        # Der Ameise wird ein neues Feld zugewiesen
+        ameise.feld = ameise_feld
+        # Jetzt legt die Ameise ihr Futter ab wenn sie das Nest erreicht hat
+        if type(ameise.feld) is Karte.Nest and ameise.futter is True:
+            ameise.futter = False
+            spielfeld.nest.gesammeltes_futter += 1
 
-def pheromon_update(spielfeld, verdunstungszeit):#Hier werden alle Pheromonpunkte der Felder reduziert, sie verdunsten
+
+# Hier werden alle Pheromonpunkte der Felder reduziert, sie verdunsten
+def pheromon_update(spielfeld, verdunstungszeit):
     for spalte in spielfeld.felder:
         for feld in spalte:
-            if feld.pheromone > 0:#Wenn die Pheromon-Punkte eines Feldes höher als ein sind
-                spielfeld.karte.itemconfigure(feld.grafik,fill="yellow")#wird das Feld in der Visualisierung gelb
-                feld.pheromone -= (1 / verdunstungszeit)#und dem Feld werden Pheromone abgezogen
-                if feld.pheromone<=0:#und wenn das Feld nun keine Pheromone mehr hat
-                    spielfeld.karte.itemconfigure(feld.grafik,fill=feld.farbe)#nimmt das Feld wieder die Farbe an die es hatte
+            # Wenn die Pheromon-Punkte eines Feldes höher als ein sind
+            if feld.pheromone > 0:
+                # wird das Feld in der Visualisierung gelb
+                spielfeld.karte.itemconfigure(feld.grafik, fill="yellow")
+                # und dem Feld werden Pheromone abgezogen
+                feld.pheromone -= (1 / verdunstungszeit)
+                # und wenn das Feld nun keine Pheromone mehr hat
+                if feld.pheromone <= 0:
+                    # nimmt das Feld wieder die Farbe an die es hatte
+                    spielfeld.karte.itemconfigure(feld.grafik, fill=feld.farbe)
 
 
 # die Funktion in der alle Ameisenobjekte erstellt werden
-def ameisenliste_fuellen(spielfeld, ameisenliste, ameisenanzahl):#der Startpunkt wird benötigt da die Ameisen immer das Feld, auf dem sie sich aufhalten, speichern
-    startpunkt=spielfeld.nest
-    karte=spielfeld.karte
-    for ameisen_nr in range(0,
-                            ameisenanzahl):  # Nun wird für jeden integer von 0 bis zur endgültigen ameisen anzahl eine Ameise erstellt
-        ameise = ameise_erstellen(startpunkt,karte)  # das Ameisenobjekt wird erstellt
-        ameisenliste.append(ameise)  # und der Ameisenliste hinzugefügt
-#Die Klasse der Ameisen mit verschiedenen Attributen
+def ameisenliste_fuellen(spielfeld, ameisenliste, ameisenanzahl):
+    # der Startpunkt wird benötigt da die Ameisen immer das Feld, auf dem sie sich aufhalten, speichern
+    startpunkt = spielfeld.nest
+    karte = spielfeld.karte
+    # Nun wird für jeden integer von 0 bis zur endgültigen ameisen anzahl eine Ameise erstellt
+    for ameisen_nr in range(0, ameisenanzahl):
+        # das Ameisenobjekt wird erstellt
+        ameise = ameise_erstellen(startpunkt, karte)
+        # und der Ameisenliste hinzugefügt
+        ameisenliste.append(ameise)
+
+
+# Die Klasse der Ameisen mit verschiedenen Attributen
 class Ameise:
-    def __init__(self, feld,karte):
+    def __init__(self, feld, karte):
         self.futter = False
         self.feld = feld
-        self.grafik = karte.create_rectangle(self.feld.k[0]+1,
-                                             self.feld.k[1]+1,
-                                             self.feld.k[2]-1,
-                                             self.feld.k[3]-1,fill="black")
+        self.grafik = karte.create_rectangle(self.feld.k[0] + 1,
+                                             self.feld.k[1] + 1,
+                                             self.feld.k[2] - 1,
+                                             self.feld.k[3] - 1, fill="black")
 
-#die Funktion, in der eine ameise erstellt wird
-def ameise_erstellen( feld,karte):
-    ameise = Ameise( feld,karte)#der Ameise werden Attriute gegeben die vorher festgelegt wurden
+
+# die Funktion, in der eine ameise erstellt wird
+def ameise_erstellen(feld, karte):
+    # der Ameise werden Attriute gegeben die vorher festgelegt wurden
+    ameise = Ameise(feld, karte)
     return ameise
 
 
-if __name__ == "__main__":  # hier wird das Progamm gestartet
-    __main__()  # indem die Funktion __main__() aufgerufen wird
+# hier wird das Progamm gestartet
+if __name__ == "__main__":
+    # indem die Funktion __main__() aufgerufen wird
+    __main__()
